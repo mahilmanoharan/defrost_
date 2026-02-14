0c08aee39633f343445d0140cde94a628ffe340f
Mahil <mahil@unc.edu>
2026-02-14 15:14:56 -0500

    Initial Commit

---
+++ defrost/defrostApp.swift
@@ -0,0 +1,17 @@
+//
+//  defrostApp.swift
+//  defrost
+//
+//  Created by Mahil Manoharan on 2/14/26.
+//
+
+import SwiftUI
+
+@main
+struct defrostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
